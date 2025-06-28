97b5483d76ff8d391a08c9f14fbee7181ae4d0ed
rvnvetri <rvnvetri@gmail.com>
2025-06-28 16:58:55 -0400

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { BrowserRouter,createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ToastProvider } from "./context/ToastContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,34 +12,38 @@ const Home = () => {
 
             
             <div className="hero flex w-full flex-col lg:flex-row">
-                <div className="card w-96 bg-base-100 card-lg shadow-sm mb-2">
+                <div className="card bg-info-content text-white  w-70 bg-base-100 card-sm shadow-sm mb-2 mr-2">
                     <div className="card-body">
                         <h2 className="card-title">STUDENT</h2>
                         <p>Login to your account with your credentials</p>
                         <div className="justify-end card-actions">
-                            <Link to="/Login">
-                                <button className="btn btn-primary text-lg">Login</button>
+                            <Link to="/Login/0">
+                                <button className="btn text-lg">Login</button>
                             </Link>
                         </div>
                     </div>
                 </div>
 
-                <div className="card w-96 bg-base-100 card-lg shadow-sm mb-2">
+                <div className="card bg-info-content text-white w-70 bg-base-50 card-sm shadow-sm mb-2 mr-2">
                     <div className="card-body">
                         <h2 className="card-title">STAFF</h2>
                         <p>Login to your account with your credentials</p>
                         <div className="justify-end card-actions">
-                            <button className="btn btn-primary text-lg">Login</button>
+                            <Link to="/Login/1">
+                                <button className="btn text-lg">Login</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
 
-                <div className="card w-96 bg-base-100 card-lg shadow-sm mb-2">
+                <div className="card bg-info-content text-white w-70 bg-base-100 card-sm shadow-sm mb-2">
                     <div className="card-body">
                         <h2 className="card-title">ADMIN</h2>
                         <p>Login to your account with your credentials</p>
                         <div className="justify-end card-actions">                            
-                            <button className="btn btn-primary text-lg">Login</button>
+                            <Link to="/Login/2">
+                                <button className="btn text-lg">Login</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
