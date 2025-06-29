2adc179b5f22f02332c81e07d6fbc02026f01fff
rvnvetri <rvnvetri@gmail.com>
2025-06-29 00:05:33 -0400

    # added files

---
+++ src/pages/StaffAuth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StaffAuth = () => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-education-700 mb-5 cfontcolor">Authentication</h1>
+     <div className="grid grid-cols-3 md:grid-cols-3 gap-3">      
+        
+      </div>              
+    </>
+  )
+}
+
+export default StaffAuth
